dfe9a810a83ff44b7ae1fc2ce0d1544842ddf74a
Gabriel Volpe <volpegabriel@gmail.com>
2018-08-12 17:12:36 +0900

    Renaming some methods. Adding Console spec.

--- core/src/main/scala/cats/effect/console/Console.scala
+++ core/src/main/scala/cats/effect/console/Console.scala
@@ -1,8 +1,7 @@
-package cats
-package effect
-package console
+package cats.effect.console
 
 import cats.Show
+import cats.effect.{IO, Sync}
 import cats.instances.string._
 import cats.syntax.show._
 
@@ -34,19 +33,19 @@ trait Console[F[_]] {
   /**
     * Prints a message of type A to the standard error output stream if an instance `Show[A]` is found.
     */
-  def error[A: Show](a: A): F[Unit]
+  def putError[A: Show](a: A): F[Unit]
 
   /**
     * Prints a message to the standard error output stream.
     */
-  final def error(str: String): F[Unit] = error[String](str)
+  final def putError(str: String): F[Unit] = putError[String](str)
 
   /**
     * Reads line from the standard console.
     *
     * @return a value representing the user's input or raise an error in the F context.
     */
-  def readLine: F[String]
+  def readLn: F[String]
 }
 object Console {
 
@@ -62,18 +61,15 @@ object Console {
   * Use it either in a DSL style:
   *
   * {{{
-  *   import cats.effect._
-  *
-  *   object io extends SyncConsole[IO]
-  *
-  *   import io._
+  *   import cats.effect.IO
+  *   import cats.effect.Console.io._
   *
   *   val program: IO[Unit] =
   *     for {
   *       _ <- putStrLn("Please enter your name: ")
-  *       n <- readLine
+  *       n <- readLn
   *       _ <- if (n.nonEmpty) putStrLn(s"Hello $$n!")
-  *            else error("Name is empty!")
+  *            else putError("Name is empty!")
   *     }
   * }}}
   *
@@ -85,9 +81,9 @@ object Console {
   *   def myProgram[F[_]](implicit C: Console[F]): F[Unit] =
   *     for {
   *       _ <- C.putStrLn("Please enter your name: ")
-  *       n <- C.readLine
+  *       n <- C.readLn
   *       _ <- if (n.nonEmpty) C.putStrLn(s"Hello $$n!")
-  *            else C.error("Name is empty!")
+  *            else C.putError("Name is empty!")
   *     }
   * }}}
   *
@@ -97,8 +93,8 @@ class SyncConsole[F[_]](implicit F: Sync[F]) extends Console[F] {
     F.delay(println(a.show))
   def putStr[A: Show](a: A): F[Unit] =
     F.delay(print(a.show))
-  def error[A: Show](a: A): F[Unit] =
+  def putError[A: Show](a: A): F[Unit] =
     F.delay(System.err.println(a.show))
-  def readLine: F[String] =
+  def readLn: F[String] =
     F.delay(scala.io.StdIn.readLine)
 }
---
+++ core/src/test/scala/cats/effect/console/ConsoleSpec.scala
@@ -0,0 +1,60 @@
+package cats.effect.console
+
+import cats.effect.IO
+import cats.effect.concurrent.Ref
+import cats.instances.boolean._
+import cats.instances.double._
+import cats.instances.int._
+import cats.syntax.all._
+import cats.{Monad, Show}
+import org.scalatest.FunSuite
+
+class ConsoleSpec extends FunSuite {
+
+  case class TestConsole(
+      out1: Ref[IO, List[String]],
+      out2: Ref[IO, List[String]],
+      out3: Ref[IO, List[String]]
+  ) extends Console[IO] {
+    override def putStrLn[A: Show](a: A): IO[Unit] =
+      out1.update(acc => acc ::: a.show :: Nil)
+    override def putStr[A: Show](a: A): IO[Unit] =
+      out2.update(acc => acc ::: a.show :: Nil)
+    override def putError[A: Show](a: A): IO[Unit] =
+      out3.update(acc => acc ::: a.show :: Nil)
+    override def readLn: IO[String] = IO.pure("test")
+  }
+
+  test("Console") {
+    def program[F[_]: Monad](implicit C: Console[F]): F[String] =
+      for {
+        _ <- C.putStrLn("a")
+        _ <- C.putStrLn(true)
+        _ <- C.putStr(123)
+        _ <- C.putStr("b")
+        n <- C.readLn
+        _ <- C.putError(s"$n")
+        _ <- C.putError(1.5)
+      } yield n
+
+    val test =
+      for {
+        out1 <- Ref.of[IO, List[String]](List.empty[String])
+        out2 <- Ref.of[IO, List[String]](List.empty[String])
+        out3 <- Ref.of[IO, List[String]](List.empty[String])
+        cio = TestConsole(out1, out2, out3)
+        rs <- program(Monad[IO], cio)
+        rs1 <- out1.get
+        rs2 <- out2.get
+        rs3 <- out3.get
+      } yield {
+        assert(rs == "test")
+        assert(rs1.mkString(",") == "a,true")
+        assert(rs2.mkString(",") == "123,b")
+        assert(rs3.mkString(",") == "test,1.5")
+      }
+
+    test.unsafeRunSync()
+  }
+
+}
