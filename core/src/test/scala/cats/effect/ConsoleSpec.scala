7a27a814bc7a06e8d8c5204b87793d15f5fdb6de
Jakub Kozłowski <kubukoz@gmail.com>
2018-11-13 22:15:55 +0100

    Expose TestConsole in artifact

--- core/src/test/scala/cats/effect/ConsoleSpec.scala
+++ core/src/test/scala/cats/effect/ConsoleSpec.scala
@@ -16,32 +16,19 @@
 
 package cats.effect
 
-import cats.data.EitherT
+import cats.data.{ Chain, EitherT }
 import cats.effect.concurrent.Ref
 import cats.instances.boolean._
 import cats.instances.double._
 import cats.instances.int._
+import cats.instances.string._
 import cats.syntax.all._
-import cats.{ Applicative, FlatMap, Show }
+import cats.FlatMap
 import org.scalatest.FunSuite
 
 class ConsoleSpec extends FunSuite {
 
-  case class TestConsole[F[_]: Applicative](
-      out1: Ref[F, List[String]],
-      out2: Ref[F, List[String]],
-      out3: Ref[F, List[String]]
-  ) extends Console[F] {
-    override def putStrLn[A: Show](a: A): F[Unit] =
-      out1.update(acc => acc ::: a.show :: Nil)
-    override def putStr[A: Show](a: A): F[Unit] =
-      out2.update(acc => acc ::: a.show :: Nil)
-    override def putError[A: Show](a: A): F[Unit] =
-      out3.update(acc => acc ::: a.show :: Nil)
-    override def readLn: F[String] = "test".pure[F]
-  }
-
-  private def program[F[_]: FlatMap](implicit C: Console[F]): F[String] =
+  def program[F[_]: FlatMap](implicit C: Console[F]): F[String] =
     for {
       _ <- C.putStrLn("a")
       _ <- C.putStrLn(true)
@@ -55,19 +42,21 @@ class ConsoleSpec extends FunSuite {
   test("Console") {
     val test =
       for {
-        out1 <- Ref.of[IO, List[String]](List.empty[String])
-        out2 <- Ref.of[IO, List[String]](List.empty[String])
-        out3 <- Ref.of[IO, List[String]](List.empty[String])
-        cio = TestConsole(out1, out2, out3)
-        rs <- program(FlatMap[IO], cio)
+        out1 <- Ref.of[IO, Chain[String]](Chain.empty[String])
+        out2 <- Ref.of[IO, Chain[String]](Chain.empty[String])
+        out3 <- Ref.of[IO, Chain[String]](Chain.empty[String])
+        rs <- {
+          implicit val console: Console[IO] = new TestConsole(out1, out2, out3)
+          program[IO]
+        }
         rs1 <- out1.get
         rs2 <- out2.get
         rs3 <- out3.get
       } yield {
         assert(rs == "test")
-        assert(rs1.mkString(",") == "a,true")
-        assert(rs2.mkString(",") == "123,b")
-        assert(rs3.mkString(",") == "test,1.5")
+        assert(rs1.mkString_("", ",", "") == "a,true")
+        assert(rs2.mkString_("", ",", "") == "123,b")
+        assert(rs3.mkString_("", ",", "") == "test,1.5")
       }
 
     test.unsafeRunSync()
@@ -78,12 +67,13 @@ class ConsoleSpec extends FunSuite {
 
     val test =
       for {
-        out1 <- Ref.of[IO, List[String]](List.empty[String])
-        out2 <- Ref.of[IO, List[String]](List.empty[String])
-        out3 <- Ref.of[IO, List[String]](List.empty[String])
+        out1 <- Ref[IO].of(Chain.empty[String])
+        out2 <- Ref[IO].of(Chain.empty[String])
+        out3 <- Ref[IO].of(Chain.empty[String])
         rs <- {
-          implicit val console =
-            TestConsole[IO](out1, out2, out3).mapK(EitherT.liftK[IO, String])
+          implicit val console: Console[E] =
+            new TestConsole[IO](out1, out2, out3)
+              .mapK(EitherT.liftK[IO, String])
           program[E].value
         }
         rs1 <- out1.get
@@ -91,9 +81,9 @@ class ConsoleSpec extends FunSuite {
         rs3 <- out3.get
       } yield {
         assert(rs.right.get == "test")
-        assert(rs1.mkString(",") == "a,true")
-        assert(rs2.mkString(",") == "123,b")
-        assert(rs3.mkString(",") == "test,1.5")
+        assert(rs1.mkString_("", ",", "") == "a,true")
+        assert(rs2.mkString_("", ",", "") == "123,b")
+        assert(rs3.mkString_("", ",", "") == "test,1.5")
       }
 
     test.value.unsafeRunSync()
