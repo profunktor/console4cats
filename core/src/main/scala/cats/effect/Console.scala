f66d05a7157261b1a32047d754c8ffce1c57b301
Jakub Kozłowski <kubukoz@gmail.com>
2018-11-16 23:18:46 +0100

    Revamp SyncConsole
    - Be more specific in Console's scaladoc, add examples
    - Move SyncConsole to a new file
    - Make input/output streams of SyncConsole customizable
    - Make SyncConsole private, provide factories

--- core/src/main/scala/cats/effect/Console.scala
+++ core/src/main/scala/cats/effect/Console.scala
@@ -16,70 +16,14 @@
 
 package cats.effect
 
-import cats.{ ~>, Show }
 import cats.instances.string._
-import cats.syntax.show._
+import cats.{ ~>, Show }
 
 /**
   * Effect type agnostic `Console` with common methods to write and read from the standard console.
-  */
-trait Console[F[_]] {
-
-  /**
-    * Prints a message of type A to the standard console followed by a new line if an instance `Show[A]` is found.
-    */
-  def putStrLn[A: Show](a: A): F[Unit]
-
-  /**
-    * Prints a message to the standard console followed by a new line.
-    */
-  def putStrLn(str: String): F[Unit] = putStrLn[String](str)
-
-  /**
-    * Prints a message of type A to the standard console if an instance `Show[A]` is found.
-    */
-  def putStr[A: Show](a: A): F[Unit]
-
-  /**
-    * Prints a message to the standard console.
-    */
-  def putStr(str: String): F[Unit] = putStr[String](str)
-
-  /**
-    * Prints a message of type A to the standard error output stream if an instance `Show[A]` is found.
-    */
-  def putError[A: Show](a: A): F[Unit]
-
-  /**
-    * Prints a message to the standard error output stream.
-    */
-  def putError(str: String): F[Unit] = putError[String](str)
-
-  /**
-    * Reads line from the standard console.
-    *
-    * @return a value representing the user's input or raise an error in the F context.
-    */
-  def readLn: F[String]
-
-  /**
-    * Transforms this console using a FunctionK.
-    * */
-  def mapK[G[_]](f: F ~> G): Console[G] = new TransformedConsole(this, f)
-}
-
-object Console {
-
-  def apply[F[_]](implicit ev: Console[F]): Console[F] = ev
-
-  /**
-    * Default instance for `Console[IO]`
-    */
-  object io extends SyncConsole[IO]
-}
-
-/**
-  * A default instance for any `F[_]` with a `Sync` instance.
+  *
+  * For an implementation that uses standard input/output,
+  * see [[Console.io]] (for `Console[IO]`) or [[SyncConsole.stdio]] (for any `F[_]: Sync`).
   *
   * Use it either in a DSL style:
   *
@@ -96,7 +40,7 @@ object Console {
   *     } yield ()
   * }}}
   *
-  * Or in tagless final encoding:
+  * Or in Tagless Final encoding:
   *
   * {{{
   *   import cats.Monad
@@ -110,19 +54,58 @@ object Console {
   *            else C.putError("Name is empty!")
   *     } yield ()
   * }}}
-  *
   */
-class SyncConsole[F[_]](implicit F: Sync[F]) extends Console[F] {
-  def putStrLn[A: Show](a: A): F[Unit] =
-    F.delay(println(a.show))
-  def putStr[A: Show](a: A): F[Unit] =
-    F.delay(print(a.show))
-  def putError[A: Show](a: A): F[Unit] =
-    F.delay(System.err.println(a.show))
-  def readLn: F[String] =
-    F.delay(scala.io.StdIn.readLine)
-  // Needed to avoid "ambiguous reference to overloaded definition" error
-  override final def putStrLn(str: String): F[Unit] = super.putStrLn(str)
-  override final def putStr(str: String): F[Unit]   = super.putStr(str)
-  override final def putError(str: String): F[Unit] = super.putError(str)
+trait Console[F[_]] {
+
+  /**
+    * Prints a message of type A to the console followed by a new line using the implicit `Show[A]` instance.
+    */
+  def putStrLn[A: Show](a: A): F[Unit]
+
+  /**
+    * Prints a message to the console followed by a new line.
+    */
+  def putStrLn(str: String): F[Unit] = putStrLn[String](str)
+
+  /**
+    * Prints a message of type A to the console using the implicit `Show[A]` instance.
+    */
+  def putStr[A: Show](a: A): F[Unit]
+
+  /**
+    * Prints a message to the console.
+    */
+  def putStr(str: String): F[Unit] = putStr[String](str)
+
+  /**
+    * Prints a message of type A to the error output using the implicit `Show[A]` instance.
+    */
+  def putError[A: Show](a: A): F[Unit]
+
+  /**
+    * Prints a message to the error output.
+    */
+  def putError(str: String): F[Unit] = putError[String](str)
+
+  /**
+    * Reads a line from the console input.
+    *
+    * @return a value representing the user's input.
+    */
+  def readLn: F[String]
+
+  /**
+    * Transforms this console using a FunctionK.
+    * */
+  def mapK[G[_]](f: F ~> G): Console[G] = new TransformedConsole(this, f)
+}
+
+object Console {
+
+  def apply[F[_]](implicit F: Console[F]): Console[F] = F
+
+  /**
+    * Default instance for `Console[IO]` that prints to standard input/output streams.
+    */
+  val io: Console[IO] = SyncConsole.stdio[IO]
 }
---
+++ core/src/main/scala/cats/effect/SyncConsole.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect
+
+import java.io.{ BufferedReader, PrintStream }
+
+import cats.Show
+import cats.syntax.show._
+
+/**
+  * A default instance for any `F[_]` with a `Sync` instance.
+  *
+  * Construct one e.g. by calling `SyncConsole.stdIn[F]`.
+  */
+private class SyncConsole[F[_]](private val out: PrintStream,
+                                private val err: PrintStream,
+                                private val in: BufferedReader)(implicit F: Sync[F])
+    extends Console[F] {
+
+  /**
+    * Prints a message of type A to the output stream followed by a new line using the implicit `Show[A]` instance.
+    * */
+  def putStrLn[A: Show](a: A): F[Unit] =
+    F.delay(out.println(a.show))
+
+  /**
+    * Prints a message of type A to the output stream using the implicit `Show[A]` instance.
+    * */
+  def putStr[A: Show](a: A): F[Unit] =
+    F.delay(out.print(a.show))
+
+  /**
+    * Prints a message of type A to the error output stream followed by a new line using the implicit `Show[A]` instance.
+    * */
+  def putError[A: Show](a: A): F[Unit] =
+    F.delay(err.println(a.show))
+
+  /**
+    * Reads a line from the console input stream.
+    *
+    * @return a value representing the user's input.
+    * */
+  def readLn: F[String] =
+    F.delay(in.readLine())
+
+  // Overrides needed to avoid "ambiguous reference to overloaded definition" error
+  override final def putStrLn(str: String): F[Unit] = super.putStrLn(str)
+  override final def putStr(str: String): F[Unit]   = super.putStr(str)
+  override final def putError(str: String): F[Unit] = super.putError(str)
+}
+
+object SyncConsole {
+
+  /**
+    * Constructs a console that prints to standard output, prints errors to standard error output, and reads from standard input.
+    *
+    * For a variant that allows customization of input/output streams, see [[make]].
+    * */
+  def stdio[F[_]: Sync]: Console[F] =
+    make(scala.Console.out, scala.Console.err, scala.Console.in)
+
+  /**
+    * Constructs a console that prints to the given `out` and `err` streams, and reads from the `in` reader.
+    *
+    * For a variant that defaults to standard input/output, see [[stdio]].
+    * */
+  def make[F[_]: Sync](out: PrintStream, err: PrintStream, in: BufferedReader): Console[F] =
+    new SyncConsole[F](out, err, in)
+}
